9add9a1c6deaeece2892c83c17d940b2c9a47f60
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2025-01-12 21:46:33 +0530

    add :adding test.sh

---
+++ section-2/01_introdution-to-terminal/app.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 5;
+
+function add(a,b){
+	console.log(a+b);
+}
+
+add(a,b);
+console.log('END');
---
+++ section-2/01_introdution-to-terminal/test.sh
@@ -0,0 +1,23 @@
+# num1=5;
+# num2=5;
+# echo  $((num1+num2))
+# # echo "end"
+# echo END
+# for i in {1..1000}; do touch "app$i.js"; done
+# for i in {1..1000}; do rm "app$i.js"; done
+
+# cmd of bash shell
+# 1. echo : it print the value work like console.log in js
+# 2. pwd : it stands for print working directory, it print the current directry path
+# 3. whoiam : it print the computer name  (current user name)
+# 4. cd : it stands for change directory
+# 5. ls : listing the content of directory
+# 6. ls -a : listing the contect of directory including hidden file
+# 7. touch : it is use to create file expample: touch index.html
+# 8. mkdir : it is used to create folder/directory . example: mkdir src
+# 9. cp : it is use to copy file and folder . example : cp app.js src
+# 10. mv : it is use to move file and folder. example : mv app.js src
+# 11. rm : it is use to delete file . example: rm app.js
+# 12. rmdir : it is use to delete empty folder/dircetory. example: rmdir src(it must directory)
+# 13. rm -r : it is use to delete folder which stands for remove recursiveley all content. example: rm -r src
+
