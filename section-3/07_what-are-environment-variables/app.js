964f426c93bb833d89950efc47dc3b85f30892db
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2025-01-15 01:15:55 +0530

    add: abc file

--- section-3/07_what-are-environment-variables/app.js
+++ section-3/07_what-are-environment-variables/app.js
@@ -1,5 +1,29 @@
 // const environmentVariables = process.env;
-// console.log(environmentVariables);
-const {exec} = require('child_process');
+//     console.log(environmentVariables);
 
-exec(`powershell -Command " setx es 'ehsan ansari' /M "`);
+
+// const environmentVariables = process.env;
+// setInterval(()=>{
+//     console.log(environmentVariables);
+    
+// },1000)
+
+
+const {exec} = require('child_process');
+// exec(`powershell -Command " setx es 'ehsan ansari' /M "`);
+
+const fs = require('fs');
+
+const fileData = fs.readFileSync('./abc').toString();
+// console.log(fileData.split('\n'));
+fileData.split(/\r?\n/).forEach(variable=>{
+   const [key,value]=variable.split('=');
+   console.log(key,value);
+    process.env[key]=value;
+   
+})
+
+
+// NODEREGEX 
+
+setInterval(()=>{},1000)
