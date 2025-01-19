a6b41ea67f2462a04a7d7a04bd44d2145ea2cbe8
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2025-01-19 22:57:23 +0530

    add: 12_npm-modules file

---
+++ section-4/11_different-types-of-modules/app.js
@@ -0,0 +1,20 @@
+// Native Modules or Core Modules
+import fs from  'node:fs'
+import net from  'node:net'
+import dgram from  'node:dgram'
+import http from  'node:http'
+import https from  'node:https'
+import child_process from  'node:child_process'
+import cluster from  'node:cluster'
+import zlib from  'node:zlib'
+import worker_thread from  'node:worker_threads'
+import crypto from  'node:crypto'
+import path from 'node:path'
+console.log(typeof fs)
+// User Modules
+import {num} from './math.js'
+
+// NPM Moudules
+import axios from 'axios';
+
+
---
+++ section-4/12_npm-modules/app.js
@@ -0,0 +1,6 @@
+// import { num } from "maths";
+const {num} = require('maths')
+
+console.log(num);
+
+//npm modules => those modules which are present in node_modules folder is called npm modules
