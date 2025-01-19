a22af9543931b55a9e80aab300d5dd597cd7a235
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2025-01-20 00:59:32 +0530

    :add 13_publishing-package-on-npm file

---
+++ section-4/13_publishing-package-on-npm/app.js
@@ -0,0 +1,6 @@
+import {num,intro,whichModule,sayHi} from 'ehsanansari'
+// const data = require('ehsanansari');
+// console.log(data);
+// data.sayHi();
+console.log(num,intro,whichModule,);
+sayHi()
