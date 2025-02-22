f22bd2329a52d8c226d34c4b114cccd9001b6e9c
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2025-02-22 19:44:28 +0530

    add:21_creating-a-web-server

--- section-10/21_creating-a-web-server/app.js
+++ section-10/21_creating-a-web-server/app.js
@@ -5,15 +5,28 @@ const server = http.createServer((request, response) =>{
   
   console.log(request.method)
   console.log(request.url)
-  if(request.url='/'){
-    const readStream = fs.createReadStream('index.html')
-    readStream.on('data',(chunk)=>{
-      response.write(chunk)
-    })
-    readStream.on('end',()=>{
-      response.end()
+  if(request.url=='/'){
+    const readStream = fs.createReadStream('./public/index.html');
+    readStream.pipe(response);
+   
+  }else{
+    const readStream = fs.createReadStream(`./public${request.url}`);
+    readStream.pipe(response)
+    readStream.on('error',(err)=>{
+      console.log(err.message)
+      response.end('not found')
     })
   }
+
+  // if(request.url='/'){
+  //   const readStream = fs.createReadStream('index.html')
+  //   readStream.on('data',(chunk)=>{
+  //     response.write(chunk)
+  //   })
+  //   readStream.on('end',()=>{
+  //     response.end()
+  //   })
+  // }
   
 })
 
