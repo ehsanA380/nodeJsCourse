de33055101ef7fdcd029527b0c9e40c246d9f6f2
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2025-03-18 00:46:43 +0530

    implement nested-directory-feature on server

--- section-11/15_adding-nested-directory-feature-server/server/app.js
+++ section-11/15_adding-nested-directory-feature-server/server/app.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import { createWriteStream } from 'fs';
+import { createWriteStream, stat } from 'fs';
 import fs from 'fs/promises'
 import cors from 'cors';
+import { dir } from 'console';
 
 const app = express()
 const port = 3000
 
 // Enabling Cors
 app.use(cors());
+app.use(express.json());
 // app.use((req, res, next) => {
 //     console.log(req.method)
 //     res.setHeader('Access-Control-Allow-Origin', '*')
@@ -18,21 +20,39 @@ app.use(cors());
 
 //serving files
 // app.use(express.static(`${import.meta.dirname}/storage`));
-app.use(async(req, res, next) => {
-    if (req.query.action === 'download') {
-        res.set('Content-Disposition', `attachment;`)
-        // res.setHeader('Content-Disposition',`attachment;`)
-    }
+// app.use(async(req, res, next) => {
+//     if (req.query.action === 'download') {
+//         res.set('Content-Disposition', `attachment;`)
+//         // res.setHeader('Content-Disposition',`attachment;`)
+//     }
     
-    express.static(`storage`)(req, res, next)
-});
+//     express.static(`storage`)(req, res, next)
+// });
 
 
 // serving directory
-app.get('/', async (req, res) => {
-    const fileItems = await fs.readdir('./storage');
-    // const stats = await 
-    res.json(fileItems)
+app.get('/directory/:dirname?', async (req, res) => {
+    const {dirname} = req.params;
+    const fullDirPath = `./storage/${dirname ? dirname : ''}`;
+    const fileItems = await fs.readdir(fullDirPath);
+    let resData = [];
+    for(const item of fileItems){
+        const stats = await fs.stat(`${fullDirPath}/${item}`);
+        // console.log(stats.isDirectory())
+        resData.push({
+            name: item,
+            isDirectory: stats.isDirectory(),
+        });
+    }
+    // console.log(resData)
+    res.json(resData)
+})
+app.get('/files/:filename',(req,res)=>{
+    const {filename} = req.params;
+    if(req.query.action==='download'){
+        res.set('Content-Disposition', `attachment;`)
+    }
+    res.sendFile(`${import.meta.dirname}/storage/${filename}`)
 })
 //upload file
 app.post('/', (req, res) => {
@@ -44,7 +64,7 @@ app.post('/', (req, res) => {
     })
 })
 //rename file
-app.put('/', (req, res) => {
+app.put('/files/:filename', (req, res) => {
 
     req.on('data', async (chunk) => {
         const data = JSON.parse(chunk.toString());
@@ -66,7 +86,7 @@ app.put('/', (req, res) => {
 
 })
 //delete file
-app.delete('/',(req,res)=>{
+app.delete('/files/:filename',(req,res)=>{
     req.on('data',async(chunk)=>{
         const deleteFilename =chunk.toString();
         try{
