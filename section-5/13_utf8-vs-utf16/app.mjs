dc35b44aa7f12e83ebf0faff2fdc4d1166035d6d
Ehsan Ansari <ehsan.ansari21@smslucknow.ac.in>
2026-05-18 17:42:00 +0530

    revised section-5

--- section-5/13_utf8-vs-utf16/app.mjs
+++ section-5/13_utf8-vs-utf16/app.mjs
@@ -1,18 +1,27 @@
+import { Buffer } from 'node:buffer';
 import fs from 'node:fs/promises';
 
 const contentBuffer = await fs.readFile('text.txt');
 
-console.log(contentBuffer);
+// console.log(contentBuffer);
+const customBuffer =Buffer.from([0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x49, 0x20, 0x61, 0x6d, 0x20, 0x45, 0x68, 0x73, 0x61, 0x6e, 0x20, 0xe2, 0x9d, 0xa4, 0xef, 0xb8, 0x8f]);
 
 function bufferToString(buffer){
-    let decimalBufferString="";
+    let hexBufferArray=[];
     let stringBuffer="";
     buffer.forEach(e => {
-        decimalBufferString +=e + ' ';
-        stringBuffer += String.fromCharCode(e);
+        hexBufferArray.push(e);
+        // stringBuffer += String.fromCharCode(e);
+        // stringBuffer += String.fromCodePoint(e);
 
+        
+        
     });
+    // Proper UTF-8 decoding
+    stringBuffer = Buffer.from(hexBufferArray).toString();
+
+    console.log('hex buffer', Buffer.from(hexBufferArray))
+
     console.log('the string of your buffer is :',stringBuffer)
 }
-
 bufferToString(contentBuffer)
